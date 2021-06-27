49deebcf774b625cedb3bf5166447697a47aed12
16630013664 <2948299576@qq.com>
2021-06-27 12:46:19 +0800

    扫描ip

--- src/main/java/com/shp/dev/chat/utils/ip/IPUtils.java
+++ src/main/java/com/shp/dev/chat/utils/ip/IPUtils.java
@@ -2,21 +2,69 @@ package com.shp.dev.chat.utils.ip;
 
 
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.*;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
-import java.util.ArrayList;
-import java.util.Enumeration;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.regex.Pattern;
 
 
 /**
  * 扫描局域网ip
  */
+@Slf4j
 public class IPUtils {
+
+
+    //private static final String  localIP = InetAddress.getLocalHost().getHostAddress();
+    private static final String localIP = AddressUtils.getInnetIp();
+    private static final String networkSegment = localIP.substring(0, localIP.lastIndexOf("."));
+
+
+    public static void main(String[] args) {
+        List<String> ips = getIpsPlus();
+        ips.forEach(System.out::println);
+    }
+
+
+    /**
+     * 获取局域网ip
+     *
+     * @return
+     */
+    @SneakyThrows
+    public static List<String> getIpsPlus() {
+        List<String> ipList = new ArrayList<>();
+        Runtime runtime = Runtime.getRuntime();
+
+        //更新trp表
+        for (int i = 1; i < 104; i++) {
+            String ip = "ping  " + networkSegment + "." + i;
+            runtime.exec(ip);
+        }
+
+        //更新trp表
+        callCmd(Objects.requireNonNull(Thread.currentThread().getContextClassLoader().getResource("")).getPath() + "bat/ping.bat");
+
+        //获取网关中的ip
+        Process process = Runtime.getRuntime().exec("arp -a");
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        bufferedReader.readLine();
+        String temp = null;
+        while (((temp = bufferedReader.readLine()) != null && !temp.isEmpty())) {
+            String ip = temp.trim().split(" ")[0];
+            if (ip.contains(networkSegment)) {
+                ipList.add(ip);
+            }
+        }
+
+        //关闭资源
+        process.destroy();
+        bufferedReader.close();
+        return ipList;
+    }
 
 
     /**
@@ -27,14 +75,9 @@ public class IPUtils {
     @SneakyThrows
     public static List<String> getIps() {
         List<String> ipList = new ArrayList<>();
-        //本地ip
-        String localIP = InetAddress.getLocalHost().getHostAddress();
-        //网段
-        String networkSegment = localIP.substring(0, localIP.lastIndexOf("."));
         //更新网段中的ip
         for (int i = 1; i < 256; i++) {
-//            String ip="ping -w 2 -n 1 " + networkSegment + "." + i;
-            String ip = "ping " + networkSegment + "." + i;
+            String ip = "ping.bat -w 2 -n 1 " + networkSegment + "." + i;
             Runtime.getRuntime().exec(ip).destroy();
         }
         //获取网关中的ip
@@ -54,12 +97,6 @@ public class IPUtils {
         return ipList;
     }
 
-    public static void main(String[] args) {
-        List<String> ips = getIps();
-        ips.forEach(System.out::println);
-
-    }
-
 
     /*
      * 判断是否为整数
@@ -68,6 +105,43 @@ public class IPUtils {
      */
     public static boolean isInteger(String str) {
         return Pattern.compile("^[-\\+]?[\\d]*$").matcher(str).matches();
+    }
+
+
+    private static void callCmd(String filePath) {
+        File file = new File(filePath);
+        if (file.isFile()) {
+            callCmd(file);
+        } else {
+            log.error("bat文件未找到");
+        }
+    }
+
+    /**
+     * 执行bat脚本
+     *
+     * @param file
+     */
+    private static void callCmd(File file) {
+        StringBuilder sb = new StringBuilder();
+        try {
+            Process child = Runtime.getRuntime().exec(file.getAbsolutePath());
+            InputStream in = child.getInputStream();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in));
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+            in.close();
+            try {
+                child.waitFor();
+            } catch (InterruptedException e) {
+                System.out.println(e);
+            }
+            log.info(sb.toString());
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
     }
 
 
